97830ff778905caf8181bda88cd3fef1c16a1903
poldeeek <pawelp258@gmail.com>
2021-06-20 20:34:28 +0200

    add svg loader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './styles.css';
 import image from './image.png';
+import svgImage from './svgImage.svg';
 
 const App = () => {
   return (
     <>
       <h1>Hello!</h1>
       <img src={image} alt='image' width='300' height='300' />
+      <img src={svgImage} alt='image' width='300' height='300' />
     </>
   );
 };
--- frontend/webpack/webpack.config.js
+++ frontend/webpack/webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         type: 'asset/resource'
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline'
       }
     ]
   },
