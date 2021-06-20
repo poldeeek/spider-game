cd69b5243f9ed6605f61ec11edbfaa8ffed2ad78
poldeeek <pawelp258@gmail.com>
2021-06-20 20:31:30 +0200

    add images loader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import './styles.css';
+import image from './image.png';
 
 const App = () => {
-  return <h1>Hello!</h1>;
+  return (
+    <>
+      <h1>Hello!</h1>
+      <img src={image} alt='image' width='300' height='300' />
+    </>
+  );
 };
 
 export default App;
