cd2ec02891a8175a0580c6f75eb90ab4e761bd89
poldeeek <pawelp258@gmail.com>
2021-06-20 20:09:27 +0200

    add file.ts for fix ts error

---
+++ frontend/file.ts
@@ -0,0 +1,3 @@
+// File for fix the error
+// tsconfig.json: Build:No inputs were found in config file
+// https://stackoverflow.com/questions/41211566/tsconfig-json-buildno-inputs-were-found-in-config-file
