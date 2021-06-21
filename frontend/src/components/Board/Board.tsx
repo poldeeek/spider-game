c1f9a21a8203bcd999b8457f1115509c9447e829
poldeeek <pawelp258@gmail.com>
2021-06-21 16:14:00 +0200

    create drawing nets component

--- frontend/src/components/Board/Board.tsx
+++ frontend/src/components/Board/Board.tsx
@@ -1,11 +1,40 @@
 import './Board.scss';
 import Spider from '../Spider/Spider';
+import Net from '../Net/Net';
 
 const Board = () => {
+  const spiders = [
+    {
+      initTop: 300, // y
+      initLeft: 400 // x
+    },
+    {
+      initTop: 50,
+      initLeft: 100
+    },
+    {
+      initTop: 50,
+      initLeft: 700
+    }
+  ];
+
+  const nets = [
+    [0, 1],
+    [0, 2],
+    [1, 2]
+  ];
+
   return (
     <div className='board'>
-      <Spider initTop={300} initLeft={400} />
-      <Spider initTop={50} initLeft={100} />
+      {spiders &&
+        spiders.map((spider, i) => (
+          <Spider key={i} initLeft={spider.initLeft} initTop={spider.initTop} />
+        ))}
+      {nets.map((net, i) => {
+        return (
+          <Net key={i} spider1={spiders[net[0]]} spider2={spiders[net[1]]} />
+        );
+      })}
     </div>
   );
 };
---
+++ frontend/src/components/Net/Net.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useRef, useState } from 'react';
+import './Net.scss';
+
+const Net: React.FC<{ spider1: any; spider2: any }> = ({
+  spider1,
+  spider2
+}) => {
+  const [style, setStyle] = useState({});
+  const myRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let x1 = spider1.initLeft;
+    let y1 = spider1.initTop;
+    let x2 = spider2.initLeft;
+    let y2 = spider2.initTop;
+
+    if (x1 > x2) {
+      let xTmp = x1;
+      let yTmp = y1;
+      x1 = x2;
+      y1 = y2;
+      x2 = xTmp;
+      y2 = yTmp;
+    }
+    if (x1 === x2) {
+      const height = Math.abs(y2 - y1).toString();
+      setStyle({
+        width: '5px',
+        left: (x1 + 75).toString() + 'px',
+        top: `${Math.min(y1, y2) + 75}px`,
+        height: `${height}px`,
+        transform: 'rotate(0deg)'
+      });
+    } else {
+      const a = (y2 - y1) / (x2 - x1);
+      const radians = Math.atan(a);
+      const degrees = radians * (180 / Math.PI);
+      const leftMove = degrees >= 0 ? 75 : 0;
+
+      setStyle({
+        width: `${Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))}px`,
+        left: (x1 + leftMove).toString() + 'px',
+        top: `${Math.min(y1, y2) + 75}px`,
+        height: `5px`,
+        transform: `rotate(${degrees}deg)`,
+        transformOrigin: `${degrees > 0 ? '0% 0%' : '100% 100%'}`
+      });
+    }
+
+    console.log(x1, y1, x2, y2);
+  }, [spider1, spider2]);
+
+  return <div className='net' style={style} ref={myRef}></div>;
+};
+
+export default Net;
