924c073f2b5399211cfd85f7a5d7146741fb2486
poldeeek <pawelp258@gmail.com>
2021-06-21 18:55:52 +0200

    add spider context provider to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import Board from './components/Board/Board';
+import { SpidersProvider } from '../context/spidersContext';
 
 const App = () => {
   return (
-    <div>
+    <SpidersProvider>
       <Board />
-    </div>
+    </SpidersProvider>
   );
 };
 
