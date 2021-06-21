bfe63306b7014f5a6f256abbb0d9543cf6b2de88
poldeeek <pawelp258@gmail.com>
2021-06-21 20:16:47 +0200

    change spider position reducer action

--- frontend/context/spidersContext.tsx
+++ frontend/context/spidersContext.tsx
@@ -1,9 +1,13 @@
 import { useReducer, Dispatch } from 'react';
 import { createContext } from 'react';
+import { CHANGE_SPIDER_POSITION } from './actionTypes';
 
 export interface ISpider {
+  id: number;
   initTop: number;
   initLeft: number;
+  x: number;
+  y: number;
 }
 
 type InitialStateType = {
@@ -13,20 +17,32 @@ type InitialStateType = {
 const initState = {
   spiders: [
     {
+      id: 0,
       initTop: 300,
-      initLeft: 400
+      initLeft: 400,
+      x: 300,
+      y: 400
     },
     {
+      id: 1,
       initTop: 50,
-      initLeft: 100
+      initLeft: 100,
+      x: 50,
+      y: 100
     },
     {
+      id: 2,
       initTop: 50,
-      initLeft: 700
+      initLeft: 700,
+      x: 50,
+      y: 700
     },
     {
+      id: 3,
       initTop: 250,
-      initLeft: 100
+      initLeft: 100,
+      x: 250,
+      y: 100
     }
   ]
 };
@@ -39,8 +55,22 @@ const SpidersContext = createContext<{
   dispatch: () => null
 });
 
-const SpidersReducer = (state: any, action: any) => {
+const SpidersReducer = (state: InitialStateType, action: any) => {
   switch (action.type) {
+    case CHANGE_SPIDER_POSITION:
+      let spidersCopy = [...state.spiders];
+
+      const foundIndex = state.spiders.findIndex(
+        (spider) => spider.id === action.payload.id
+      );
+
+      spidersCopy[foundIndex] = {
+        ...spidersCopy[foundIndex],
+        x: action.payload.x,
+        y: action.payload.y
+      };
+
+      return { ...state, spiders: [...spidersCopy] };
     default:
       return state;
   }
