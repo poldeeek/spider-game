8a7bf152a2cc6226fa5dc5d8bc675cbfa7eaff7f
poldeeek <pawelp258@gmail.com>
2021-06-22 23:26:12 +0200

    add timer to header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import './Header.scss';
 import RoundContext from '../../context/roundContext';
+import Timer from '../Timer/Timer';
 
 const Header = () => {
   const { roundState, roundDispatch } = useContext(RoundContext);
@@ -9,6 +10,7 @@ const Header = () => {
   return (
     <header>
       <h1>Level {round}</h1>
+      <Timer />
     </header>
   );
 };
