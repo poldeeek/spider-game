0e99d02111881cf8d2c0234cbba24d8df2effb4c
poldeeek <pawelp258@gmail.com>
2021-06-22 23:28:59 +0200

    extract reducer to another file

--- frontend/src/context/roundContext.tsx
+++ frontend/src/context/roundContext.tsx
@@ -1,9 +1,7 @@
 import { useReducer, Dispatch } from 'react';
 import { createContext } from 'react';
-import checkIntersections from '../helpers/checkIntersection';
-import * as actions from './actionTypes';
-
 import roundsData from '../roundsData/rounds';
+import RoundReducer from './roundReducer';
 
 export interface ISpider {
   id: number;
@@ -44,85 +42,6 @@ const RoundContext = createContext<{
   roundState: initState,
   roundDispatch: () => null
 });
-
-const RoundReducer = (state: RoundStateType, action: any) => {
-  switch (action.type) {
-    case actions.CHANGE_SPIDER_POSITION:
-      const foundIndex = state.spiders.findIndex(
-        (spider) => spider.id === action.payload.id
-      );
-
-      const updatedSpider = {
-        ...state.spiders[foundIndex],
-        x: action.payload.x,
-        y: action.payload.y
-      };
-
-      return {
-        ...state,
-        isComplete: false,
-        spiders: [
-          ...state.spiders.slice(0, foundIndex),
-          updatedSpider,
-          ...state.spiders.slice(foundIndex + 1)
-        ]
-      };
-    case actions.CHECK_INTERSECTION:
-      const checkedNets = checkIntersections(state);
-      return {
-        ...state,
-        nets: [...checkedNets]
-      };
-    case actions.CHECK_IS_COMPLETED:
-      const failIntersection = state.nets.find((net) => net.isIntersection);
-      if (!failIntersection) {
-        return {
-          ...state,
-          isComplete: true,
-          showCompletePopup: true,
-          error: ''
-        };
-      }
-      return state;
-    case actions.REPLAY:
-      return {
-        ...state,
-        ...roundsData[state.round - 1],
-        showCompletePopup: false,
-        error: '',
-        time: ''
-      };
-    case actions.NEXT_ROUND:
-      const nextRound = state.round + 1;
-      if (nextRound > state.roundsNumber)
-        return { ...state, error: 'That was the last round!' };
-      return {
-        ...state,
-        ...roundsData[nextRound - 1],
-        round: nextRound,
-        showCompletePopup: false,
-        error: '',
-        time: ''
-      };
-    case actions.PREV_ROUND:
-      const prevRound = state.round - 1;
-      if (prevRound < 1) {
-        return { ...state, error: 'This is the first round!' };
-      }
-      return {
-        ...state,
-        ...roundsData[prevRound - 1],
-        round: prevRound,
-        showCompletePopup: false,
-        error: '',
-        time: ''
-      };
-    case 'TIME_STOP':
-      return { ...state, time: action.payload.time };
-    default:
-      return state;
-  }
-};
 
 const RoundProvider: React.FC = ({ children }) => {
   const [state, dispatch] = useReducer(RoundReducer, initState);
---
+++ frontend/src/context/roundReducer.tsx
@@ -0,0 +1,85 @@
+import checkIntersections from '../helpers/checkIntersection';
+import * as actions from './actionTypes';
+import { RoundStateType } from './roundContext';
+import roundsData from '../roundsData/rounds';
+
+const RoundReducer = (state: RoundStateType, action: any) => {
+  switch (action.type) {
+    case actions.CHANGE_SPIDER_POSITION:
+      const foundIndex = state.spiders.findIndex(
+        (spider) => spider.id === action.payload.id
+      );
+
+      const updatedSpider = {
+        ...state.spiders[foundIndex],
+        x: action.payload.x,
+        y: action.payload.y
+      };
+
+      return {
+        ...state,
+        isComplete: false,
+        spiders: [
+          ...state.spiders.slice(0, foundIndex),
+          updatedSpider,
+          ...state.spiders.slice(foundIndex + 1)
+        ]
+      };
+    case actions.CHECK_INTERSECTION:
+      const checkedNets = checkIntersections(state);
+      return {
+        ...state,
+        nets: [...checkedNets]
+      };
+    case actions.CHECK_IS_COMPLETED:
+      const failIntersection = state.nets.find((net) => net.isIntersection);
+      if (!failIntersection) {
+        return {
+          ...state,
+          isComplete: true,
+          showCompletePopup: true,
+          error: ''
+        };
+      }
+      return state;
+    case actions.REPLAY:
+      return {
+        ...state,
+        ...roundsData[state.round - 1],
+        showCompletePopup: false,
+        error: '',
+        time: ''
+      };
+    case actions.NEXT_ROUND:
+      const nextRound = state.round + 1;
+      if (nextRound > state.roundsNumber)
+        return { ...state, error: 'That was the last round!' };
+      return {
+        ...state,
+        ...roundsData[nextRound - 1],
+        round: nextRound,
+        showCompletePopup: false,
+        error: '',
+        time: ''
+      };
+    case actions.PREV_ROUND:
+      const prevRound = state.round - 1;
+      if (prevRound < 1) {
+        return { ...state, error: 'This is the first round!' };
+      }
+      return {
+        ...state,
+        ...roundsData[prevRound - 1],
+        round: prevRound,
+        showCompletePopup: false,
+        error: '',
+        time: ''
+      };
+    case 'TIME_STOP':
+      return { ...state, time: action.payload.time };
+    default:
+      return state;
+  }
+};
+
+export default RoundReducer;
